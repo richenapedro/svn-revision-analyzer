b2b9471a96a2b0d9c013cdc41b2c40dd8b3a5c81
Pedro Richena <richenapedro@gmail.com>
2026-07-06 13:40:48 +0200

    Add changed path existence helpers

--- svn/models.py
+++ svn/models.py
@@ -1,10 +1,23 @@
 from dataclasses import dataclass
+from pathlib import Path
 
 
 @dataclass(slots=True)
 class ChangedPath:
     path: str
     action: str
+
+    @property
+    def relative_path(self) -> Path:
+        return Path(self.path.lstrip("/"))
+
+    @property
+    def exists_before(self) -> bool:
+        return self.action not in {"A"}
+
+    @property
+    def exists_after(self) -> bool:
+        return self.action not in {"D"}
 
 
 @dataclass(slots=True)
